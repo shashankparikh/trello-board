e68bbb03a7806acde835731f9afd87ae2bfbf051
shashank <shashank.parikh@nobroker.in>
2021-08-17 09:21:42 +0530

    first commit

--- src/components/common/SdRadio/sdRadio.js
+++ src/components/common/SdRadio/sdRadio.js
@@ -1,28 +1,54 @@
-import React from 'react'
-import Radio from '@material-ui/core/Radio';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import React from "react";
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 const SdRadio = (props) => {
-    const {type,checked,disabled,onChange,required,className,id,text,value,color,name} = props;
-    return (
-         <FormControlLabel className={className} control={<Radio color={color} onChange={onChange} checked={checked} id={id} disabled={disabled} value={value} name={name || 'name'} required={required}/>} label={text || 'Radio Button'} />
-    )
-}
+  const {
+    type,
+    checked,
+    disabled,
+    onChange,
+    required,
+    className,
+    id,
+    text,
+    value,
+    color,
+    name,
+  } = props;
+  return (
+    <FormControlLabel
+      className={className}
+      control={
+        <Radio
+          color={color}
+          onChange={onChange}
+          checked={checked}
+          id={id}
+          disabled={disabled}
+          value={value}
+          name={name || "name"}
+          required={required}
+        />
+      }
+      label={text || "Radio Button"}
+    />
+  );
+};
 
-SdRadio.defaultProps={
-    // checked: false,
-    readOnly: false,
-    disabled: false,
-    inline: false,
-    onChange: () => {},
-    validationState: null,
-    id: '',
-    name: '',
-    value: null,
-    containerClass: '',
-    color: 'primary',
-    type: 'radio'
-}
+SdRadio.defaultProps = {
+  // checked: false,
+  readOnly: false,
+  disabled: false,
+  inline: false,
+  onChange: () => {},
+  validationState: null,
+  id: "",
+  name: "",
+  value: null,
+  containerClass: "",
+  color: "primary",
+  type: "radio",
+};
 
-
-export default SdRadio
+export default SdRadio;
